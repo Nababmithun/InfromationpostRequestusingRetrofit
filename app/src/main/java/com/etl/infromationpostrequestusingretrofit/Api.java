9ff4ca2e33818ad2954fbbbf005c417ca34e35d5
Mithun <monermithun789@gmail.com>
2019-11-24 09:46:45 -0800

    Initial commit

---
+++ app/src/main/java/com/etl/infromationpostrequestusingretrofit/Api.java
@@ -0,0 +1,21 @@
+package com.etl.infromationpostrequestusingretrofit;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface Api {
+
+
+
+    @FormUrlEncoded
+    @POST("createuser")
+    Call<DefaultResponse> createUser(
+            @Field("email") String email,
+            @Field("password") String password,
+            @Field("name") String name,
+            @Field("school") String school
+    );
+}
